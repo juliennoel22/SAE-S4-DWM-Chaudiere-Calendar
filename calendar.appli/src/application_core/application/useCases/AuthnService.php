95fad5348778da167cab381ea22bb430807492dc
Guillaume <guillaume.hess9@etu.univ-lorraine.fr>
2025-06-11 09:07:04 +0200

    correction twig

--- calendar.appli/src/application_core/application/useCases/AuthnService.php
+++ calendar.appli/src/application_core/application/useCases/AuthnService.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1); 
 
-namespace calendar\core\application_core\application\usecase;
+namespace calendar\core\application_core\application\useCases;
 
 use calendar\core\application_core\application\exceptions\AuthnException;
 use calendar\core\application_core\application\entities\User;
--- calendar.appli/src/application_core/application/useCases/AuthnServiceInterface.php
+++ calendar.appli/src/application_core/application/useCases/AuthnServiceInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace calendar\core\application_core\application\usecase;
+namespace calendar\core\application_core\application\useCases;
 
 interface AuthnServiceInterface
 {
