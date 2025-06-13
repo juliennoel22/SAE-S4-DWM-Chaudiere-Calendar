f9ebd81ef193c49341f86510958ca014e7eedfeb
juliennoel <julien.n2208@gmail.com>
2025-06-13 11:40:57 +0200

    Ajout de l'API d'index et mise à jour des routes pour une meilleure documentation des endpoints

---
+++ calendar.appli/src/webui/actions/ApiIndexAction.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace calendar\core\webui\actions;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+final class ApiIndexAction
+{
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'api_index.twig');
+    }
+}
