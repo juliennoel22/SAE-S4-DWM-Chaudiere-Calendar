621fde699a4df2b7fd685a4bdd59717e92363e0d
juliennoel <julien.n2208@gmail.com>
2025-06-13 16:30:16 +0200

    correction route api js localhost

--- calendar.appli.web/js/api.js
+++ calendar.appli.web/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'http://localhost:8888/api';
 // const API_BASE_URL = 'http://docketu.iutnc.univ-lorraine.fr:8888/api';
 
 async function fetchFromApi(endpoint) {
